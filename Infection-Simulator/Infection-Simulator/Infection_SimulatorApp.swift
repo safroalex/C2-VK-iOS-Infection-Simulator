069e73208f0933c25329cdd0a6b04b68641aab71
safroalex <alexsafr.a@ya.ru>
2024-03-22 13:25:46 +0300

    init proj

---
+++ Infection-Simulator/Infection-Simulator/Infection_SimulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Infection_SimulatorApp.swift
+//  Infection-Simulator
+//
+//  Created by Александр Сафронов on 22.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Infection_SimulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
