4a4b16f79086d3d19ca6828511dd2e943372c24e
safroalex <alexsafr.a@ya.ru>
2024-03-22 14:08:44 +0300

    good proj on UIKit without storyboard

---
+++ VK-iOS-Infection-Simulator/VK-iOS-Infection-Simulator/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  VK-iOS-Infection-Simulator
+//
+//  Created by Александр Сафронов on 22.03.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.windowScene = windowScene
+        window?.makeKeyAndVisible()
+        window?.rootViewController = ViewController()
+    }
+}
+
---
+++ VK-iOS-Infection-Simulator/VK-iOS-Infection-Simulator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VK-iOS-Infection-Simulator
+//
+//  Created by Александр Сафронов on 22.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        view.backgroundColor = .systemTeal
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
